869c41cecc5b334db9703c99e3df2e9d85285328
yz3gn <yz3gn@mathcs02.richmond.edu>
2018-11-01 18:38:03 -0400

    correct version

--- DependencyChecker.cpp
+++ DependencyChecker.cpp
@@ -44,13 +44,9 @@ void DependencyChecker::addInstruction(Instruction i)
     rs = i.getRS();
     rt = i.getRT();
     if(i.getImmediate() != -1){
-      checkForReadDependence(rs);
-      checkForReadDependence(rt);
-    } else{
-      checkForReadDependence(rs);
-      checkForWriteDependence(rt);
+        checkForReadDependence(rs);
+        checkForWriteDependence(rt);
     }
-
     break;
 
   case JTYPE:
@@ -70,24 +66,19 @@ void DependencyChecker::checkForReadDependence(unsigned int reg)
    * the appropriate RegisterInfo entry regardless of dependence detection.
    */
 { //check for the read dependence occur when reg is read
-   list<Instruction>::iterator it;
-   int i = myInstructions.size(); // current instruction number   
+   if(reg >= 0 && reg < NumRegisters){
+   int i = myInstructions.size();               
+	     if(myCurrentState[reg].accessType == WRITE)
+                 addDependEntry(reg, RAW);
+      
 
-   for(it = myInstructions.begin(); it !=  myInstructions.end(); it++){
-      if((*it).getInstType() == RTYPE){
-         if(reg == (*it).getRD())
-		addDependEntry(reg,RAW);
-      } else if((*it).getInstType() == ITYPE){
-	         if(reg == (*it).getRT()) {
-	           addDependEntry(reg,RAW);	
-           }
-      }
-    }
+   
    //update the RegisterInfo entry              
-   RegisterInfo re;
-   re.lastInstructionToAccess = i;
-   re.accessType = READ;
-   myCurrentState.at(reg) = re;
+   RegisterInfo updateInfo;
+   updateInfo.lastInstructionToAccess = i;
+   updateInfo.accessType = READ;
+   myCurrentState.at(reg) = updateInfo;
+}
  }     
        
 
@@ -97,13 +88,14 @@ void DependencyChecker::addDependEntry(unsigned int reg, DependenceType type)
     Dependence dep;
     dep.dependenceType = type;
     dep.registerNumber = reg;
-    dep.previousInstructionNumber = myCurrentState.at(reg).lastInstructionToAccess;// what this should be?
+    dep.previousInstructionNumber = myCurrentState.at(reg).lastInstructionToAccess;
     dep.currentInstructionNumber = myInstructions.size() ;
-    cout << "previous instruction num" << myCurrentState.at(reg).lastInstructionToAccess << endl; 
-    cout << "crrent instrunction num " << myInstructions.size()  << endl;
     myDependences.push_back(dep);
-  //}
+
+       
 }
+
+
 
 
 
@@ -112,37 +104,21 @@ void DependencyChecker::checkForWriteDependence(unsigned int reg)
    * instruction.  If so, adds an entry to the list of dependences. Also updates 
    * the appropriate RegisterInfo entry regardless of dependence detection.
    */
-{
-   list<Instruction>::iterator it;
-   int i = myInstructions.size();
+   {
+   if(reg >= 0 && reg < NumRegisters){
+      int i = myInstructions.size();
+   if(myCurrentState[reg].accessType == WRITE)
+       addDependEntry(reg,WAW);
+   if(myCurrentState[reg].accessType == READ)
+       addDependEntry(reg,WAR);
    
-   for(it = myInstructions.begin(); it !=  myInstructions.end(); it++){
-      if((*it).getInstType() == RTYPE){
-         if((int)reg == (*it).getRD()){
-                addDependEntry(reg,WAW);
-          }
-         else if((int)reg == (*it).getRS() ||(int)reg == (*it).getRT()) {
-	        addDependEntry(reg,WAR);
-         }
-
-      } else if((*it).getInstType() == ITYPE){
-        if(reg == (*it).getRT()){
-                addDependEntry(reg,WAW);
-        }
-	else if(reg == (*it).getRS()) {
-	        addDependEntry(reg,WAR);
-	}  
-      }
-   }
-   
-    RegisterInfo re;
-    re.lastInstructionToAccess = i;
-    re.accessType = WRITE;
-    myCurrentState.at(reg) = re;
-    cout << "myCurrentState at(reg): " << myCurrentState.at(reg).lastInstructionToAccess << endl; 
+    RegisterInfo updateInfo;
+    updateInfo.lastInstructionToAccess = i;
+    updateInfo.accessType = WRITE;
+    myCurrentState.at(reg) = updateInfo;
   
   }
-
+}
 void DependencyChecker::printDependences()
   /* Prints out the sequence of instructions followed by the sequence of data
    * dependencies.
